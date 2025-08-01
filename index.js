8af27d6c606578f31b00e4b09f64000a4479e171
CBeckJr96 <c.beckwood.jr@gmail.com>
2025-07-31 22:06:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const token = process.env.discord_token;
+const token = process.env.DISCORD_TOKEN;
 client.login(token);
 const { Client, GatewayIntentBits } = require('discord.js');
 const express = require('express');
